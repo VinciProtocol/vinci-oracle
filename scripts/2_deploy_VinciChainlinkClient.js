97f5a1911c6bd6d7df0f6e22ac5d83beaf0da6db
Omie-Gao <yes.omg001@gmail.com>
2022-05-10 13:43:54 +0800

    refactor: deployment scripts

---
+++ scripts/2_deploy_VinciChainlinkClient.js
@@ -0,0 +1,46 @@
+const {
+    Context
+} = require('./context');
+
+
+async function main() {
+    await Context(async (context, network) => {
+        const address = process.env.VINCI_COLLECT_PRICE_CUMULATIVE_ADDRESS;
+        const linkTokenAddress = process.env.LINK_ADDRESS;
+        if (!address || !linkTokenAddress) {
+            throw new Error("VINCI_COLLECT_PRICE_CUMULATIVE_ADDRESS and LINK_ADDRESS must be provided in env.");
+        };
+
+        const vinciCollectPriceCumulative = await context.getContract(
+            'VinciCollectPriceCumulative',
+            address,
+        );
+        const client = await context.deployContract("VinciChainlinkClient", linkTokenAddress);
+
+        console.log("🔨 setNode ...");
+        await client.setNode(
+            vinciCollectPriceCumulative.address,
+            process.env.ORACLE_ADDRESS,
+            process.env.JOBID,
+            BigInt(process.env.FEE * 10 ** 18),
+            process.env.REQUEST_URL,
+            process.env.REQUEST_PATH,
+        );
+        console.log(
+            "✔️  VinciChainlinkClient setNode:",
+            "\n  vinciCollectPriceCumulative:", vinciCollectPriceCumulative.address,
+            "\n  oracle: ", process.env.ORACLE_ADDRESS,
+            "\n  jobId: ", process.env.JOBID,
+            "\n  fee: ", process.env.FEE,
+            "\n  url: ", process.env.REQUEST_URL,
+            "\n  path: ", process.env.REQUEST_PATH,
+        );
+
+        console.log("🔨 transferOperationRight ...");
+        await vinciCollectPriceCumulative.transferOperationRight(client.address);
+        console.log(`✔️  Set VinciCollectPriceCumulative operator: ${client.address}.`);
+
+
+    });
+}
+main();
